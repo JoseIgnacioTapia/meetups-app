97c4f262fabc23b02a4aa3e814fde4509a340c47
JoseIgnacioTapia <josignaciot@gmail.com>
2021-11-11 17:33:44 -0300

    Adding Custom Components & CSS Modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Prague_Cubim_Adria_Palace.jpg/1024px-Prague_Cubim_Adria_Palace.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
