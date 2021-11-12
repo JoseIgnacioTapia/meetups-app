cbb4105f60656247586477ffe1e7a389711e3eb7
JoseIgnacioTapia <josignaciot@gmail.com>
2021-11-12 15:53:34 -0300

    Working with Params for SSG Data Fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,25 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticProps() {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Prague_Cubim_Adria_Palace.jpg/1024px-Prague_Cubim_Adria_Palace.jpg',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Some Street 5 Some City',
+        description: 'This is a first meetup',
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
