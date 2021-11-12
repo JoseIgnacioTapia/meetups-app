d95379fa24ee2788ce60f8d3d099518ef8111c66
JoseIgnacioTapia <josignaciot@gmail.com>
2021-11-12 20:11:43 -0300

    Working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,25 @@
+import { MongoClient } from 'mongodb';
+
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      'mongodb+srv://nchmn907:nacho2200@cluster0.gev1u.mongodb.net/meetsup?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
